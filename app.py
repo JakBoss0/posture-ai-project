edabd0b3842c3fec0348209aa9e30a3a1624b030
Jakkapong Hathaitad <boss0637432964@gmail.com>
2026-03-30 17:15:29 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import pickle
+
+# โหลด model
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = Flask(__name__)
+
+# test route
+@app.route("/")
+def home():
+    return "Posture AI Backend Running!"
+
+# predict route
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+
+    left = data.get("left")
+    right = data.get("right")
+    back = data.get("back")
+
+    # เช็ค input
+    if left is None or right is None or back is None:
+        return jsonify({"error": "Missing data"}), 400
+
+    # predict
+    prediction = model.predict([[left, right, back]])
+
+    return jsonify({
+        "posture": prediction[0]
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
